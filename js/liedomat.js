7df973d002360a2336bbd00e4bc49fda15986788
Tobias Zysk <tobias.zysk@fh-muenster.de>
2018-07-05 22:23:01 +0200

    Implemented saving song to table

--- js/liedomat.js
+++ js/liedomat.js
@@ -6,10 +6,37 @@ function typeContent(char) {
     let versesText = pVerses.text();
 
     let numberLen = numberText.length;
-    let versesLen = versesText.length;
 
-    if (char === 'L') {
+    if (numberText.match(/[a-z]/i)) {
+        numberText = '';
+        numberLen = numberText.length;
         pNumber.text('');
+    }
+
+    if (char === '<') {
+        position--;
+        position = (position+13) % 13;
+        pNumber.text(songlist[position].label);
+    }
+    else if (char === '>') {
+        position++;
+        position = position % 13;
+        pNumber.text(songlist[position].label);
+    }
+    else if (char === 'A') {
+        let saveNumber = '';
+        versesText.length > 0 ? saveNumber = numberText.concat(', ', versesText) : saveNumber = numberText;
+        if (saveNumber.charAt(0) === '0') { saveNumber = saveNumber.slice(1,saveNumber.length); }
+
+        songlist[position].number = saveNumber;
+        position++;
+        position = position % 13;
+
+        pNumber.text(songlist[position].label);
+        pVerses.text('')
+        setFields();
+    } else if (char === 'L') {
+        pNumber.text(songlist[position].label);
         pVerses.text('')
     } else if (numberLen < 4) {
         numberText += char;
@@ -58,7 +85,44 @@ $(document).ready(() => {
     $('#minusBtn').click(() => { typeContent('-') });
     $('#plusBtn').click(() => { typeContent('+') });
     $('#delBtn').click(() => { typeContent('L') });
+    $('#setBtn').click(() => { typeContent('A') });
+    $('#nextBtn').click(() => { typeContent('>') });
+    $('#backBtn').click(() => { typeContent('<') });
+
+    setFields();
 });
+
+function setFields() {
+    $('#label1').text(songlist[0].label + ':');
+    $('#label2').text(songlist[1].label + ':');
+    $('#label3').text(songlist[2].label + ':');
+    $('#label4').text(songlist[3].label + ':');
+    $('#label5').text(songlist[4].label + ':');
+    $('#label6').text(songlist[5].label + ':');
+    $('#label7').text(songlist[6].label + ':');
+    $('#label8').text(songlist[7].label + ':');
+    $('#label9').text(songlist[8].label + ':');
+    $('#label10').text(songlist[9].label + ':');
+    $('#label11').text(songlist[10].label + ':');
+    $('#label12').text(songlist[11].label + ':');
+    $('#label13').text(songlist[12].label + ':');
+
+    $('#number1').text(songlist[0].number);
+    $('#number2').text(songlist[1].number);
+    $('#number3').text(songlist[2].number);
+    $('#number4').text(songlist[3].number);
+    $('#number5').text(songlist[4].number);
+    $('#number6').text(songlist[5].number);
+    $('#number7').text(songlist[6].number);
+    $('#number8').text(songlist[7].number);
+    $('#number9').text(songlist[8].number);
+    $('#number10').text(songlist[9].number);
+    $('#number11').text(songlist[10].number);
+    $('#number12').text(songlist[11].number);
+    $('#number13').text(songlist[12].number);
+
+    $('#display-number').text(songlist[position].label);
+}
 
 $(document).on('keyup', (event) => {
     let char = String.fromCharCode(event.keyCode);
@@ -72,9 +136,15 @@ $(document).on('keyup', (event) => {
         char = '+';
     }
     if (event.keyCode === 189) { // -
-        char = '-'
+        char = '-';
     }
-    let allowed = /[0-9L+\-]/g.test(char);
+    if (event.keyCode === 37) { // <-
+        char = "<";
+    }
+    if (event.keyCode === 39) { // ->
+        char = ">";
+    }
+    let allowed = /[0-9AL+\-<>]/g.test(char);
     if (allowed) {
         typeContent(char);
     }
