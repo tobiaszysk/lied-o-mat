5ff0862ea1890f6eb617d7520c46dda2b032ab2b
Tobias Zysk <tobias.zysk@fh-muenster.de>
2018-07-09 23:08:27 +0200

    added print window

--- js/liedomat.js
+++ js/liedomat.js
@@ -71,6 +71,141 @@ function typeContent(char) {
     }
 }
 
+function makePrintPage() {
+    let header = `<meta charset="utf-8">
+    <meta name="viewport" value="width=device-width, initial-scale=1">
+    <meta name="author" value="Tobias Zysk">
+    <meta name="generator" value="JavaScript">
+    <title>Gottesdienstgestaltung - Exportvorlage</title>
+
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css">
+
+    <!-- jQuery library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+    <!-- Popper JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"></script>
+
+    <!-- Latest compiled JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"></script>
+    
+    <style>
+        @Page Content {
+            size: 21cm 29.7cm;
+            margin: 10mm 10mm 10mm 10mm;
+        }
+
+        body {
+            page: Content;
+        }
+
+        td {
+            padding-bottom: 1.5em;
+        }
+    </style>`;
+
+    let layout = `
+    <div class="container">
+        <div class="row">
+            <div id="topleft" class="col-md-6"></div>
+            <div id="topright" class="col-md-6"></div>
+        </div>
+        <div class="row">
+            <div id="bottomleft" class="col-md-6"></div>
+            <div id="bottomright" class="col-md-6"></div>
+        </div>
+    </div>
+    `;
+
+    let template = `
+    <div class="card">
+    <div class="card-header">
+        <strong>Gottesdienstgestaltung</strong>
+    </div>
+    <div class="card-body">
+        <table>
+            <tbody>
+                <tr>
+                    <td>${songlist[0].label}</td>
+                    <td>${songlist[0].number}</td>
+                    <td>${songlist[0].comment}</td>
+                </tr>
+                <tr>
+                    <td>${songlist[1].label}</td>
+                    <td>${songlist[1].number}</td>
+                    <td>${songlist[1].comment}</td>
+                </tr>
+                <tr>
+                    <td>${songlist[2].label}</td>
+                    <td>${songlist[2].number}</td>
+                    <td>${songlist[2].comment}</td>
+                </tr>
+                <tr>
+                    <td>${songlist[3].label}</td>
+                    <td>${songlist[3].number}</td>
+                    <td>${songlist[3].comment}</td>
+                </tr>
+                <tr>
+                    <td>${songlist[4].label}</td>
+                    <td>${songlist[4].number}</td>
+                    <td>${songlist[4].comment}</td>
+                </tr>
+                <tr>
+                    <td>${songlist[5].label}</td>
+                    <td>${songlist[5].number}</td>
+                    <td>${songlist[5].comment}</td>
+                </tr>
+                <tr>
+                    <td>${songlist[6].label}</td>
+                    <td>${songlist[6].number}</td>
+                    <td>${songlist[6].comment}</td>
+                </tr>
+                <tr>
+                    <td>${songlist[7].label}</td>
+                    <td>${songlist[7].number}</td>
+                    <td>${songlist[7].comment}</td>
+                </tr>
+                <tr>
+                    <td>${songlist[8].label}</td>
+                    <td>${songlist[8].number}</td>
+                    <td>${songlist[8].comment}</td>
+                </tr>
+                <tr>
+                    <td>${songlist[9].label}</td>
+                    <td>${songlist[9].number}</td>
+                    <td>${songlist[9].comment}</td>
+                </tr>
+                <tr>
+                    <td>${songlist[10].label}</td>
+                    <td>${songlist[10].number}</td>
+                    <td>${songlist[10].comment}</td>
+                </tr>
+                <tr>
+                    <td>${songlist[11].label}</td>
+                    <td>${songlist[11].number}</td>
+                    <td>${songlist[11].comment}</td>
+                </tr>
+                <tr>
+                    <td>${songlist[12].label}</td>
+                    <td>${songlist[12].number}</td>
+                    <td>${songlist[12].comment}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+    </div>`;
+
+    let doc = window.open('', 'Gottesdienstgestaltung - Exportformat', `toolbar=no,location=no,directories=no,resizeable=yes,width=800, height=100, top=10, left=10`);
+    doc.document.head.innerHTML = header;
+    doc.document.body.innerHTML = layout;
+    doc.document.getElementById('topleft').innerHTML = template;
+    doc.document.getElementById('topright').innerHTML = template;
+    doc.document.getElementById('bottomleft').innerHTML = template;
+    doc.document.getElementById('bottomright').innerHTML = template;
+    doc.print();
+}
+
 $(document).ready(() => {
     $('#oneBtn').click(() => { typeContent('1') });
     $('#twoBtn').click(() => {typeContent('2') });
@@ -88,6 +223,7 @@ $(document).ready(() => {
     $('#setBtn').click(() => { typeContent('A') });
     $('#nextBtn').click(() => { typeContent('>') });
     $('#backBtn').click(() => { typeContent('<') });
+    $('#exportBtn').click(() => { makePrintPage() });
 
     setFields();
 });
